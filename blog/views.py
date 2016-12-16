efcf6ecd5ce5a47c9916f958aed10ae2a5babc5b
litunny0xf <liquidcoding2009@gmail.com>
2016-12-16 14:48:41 +0100

    Change the HTML files for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
