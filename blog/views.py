d35428ce1c1b6fe49a8d5fd8bac2f610d858af7a
litunny0xf <liquidcoding2009@gmail.com>
2016-12-16 22:09:28 +0100

    Implement query set

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-# Create your views here.
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {'posts' : posts})
