3957cfbf4c1f545a953f112b5500d1c3a99755ef
litunny0xf <liquidcoding2009@gmail.com>
2016-12-17 09:31:12 +0100

    edit contact error fixed

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^(?P<pk>\d+)/$', views.contact_view, name="contact_view"),
     url(r'^new/$', views.contact_new, name="contact_new"),
+    url(r'^edit/(?P<pk>\d+)$', views.contact_edit, name="contact_edit"),
 ]
--- contact/views.py
+++ contact/views.py
@@ -29,3 +29,21 @@ def contact_new(request):
         form = ContactForm()
 
     return render(request, 'contact/new.html', {'form' : form})
+
+def contact_edit (request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+            form = ContactForm(request.POST, instance=contact)
+            if form.is_valid():
+                contact = form.save(commit=False)
+                contact.first_name = request.POST['first_name']
+                contact.middle_name = request.POST['middle_name']
+                contact.last_name = request.POST['last_name']
+                contact.phone_number = request.POST['phone_number']
+                contact.active = True
+                contact.save()
+                return redirect('index')
+    else:
+        form = ContactForm(instance=contact)
+
+    return render(request, 'contact/edit.html', {'form' : form, 'contact' : contact})
